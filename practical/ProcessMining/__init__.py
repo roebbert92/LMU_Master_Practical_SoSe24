0fec3413ed59d7ac47f0b939d7d53f545c535970
Collin Leiber <leiber@dbs.ifi.lmu.de>
2024-04-19 16:43:33 +0200

    init comit

---
+++ practical/ProcessMining/__init__.py
@@ -0,0 +1,3 @@
+from .dummy import hello_world
+
+__all__ = ["hello_world"]
---
+++ practical/ProcessMining/tests/test_dummy.py
@@ -0,0 +1,5 @@
+from practical.ProcessMining import hello_world
+
+
+def test_hello_world():
+    assert hello_world("Process Mining") == "Hello Process Mining"
