7c0077d16d0b010249e59f426a4a1aa26c5c22c9
jian7star <131192282+jian7star@users.noreply.github.com>
2024-04-28 21:11:46 +0200

    change

--- practical/DeepClustering/deep_clustering_dummy.py
+++ practical/DeepClustering/deep_clustering_dummy.py
@@ -1,2 +1,27 @@
-def hello_world(string: str) -> str:
-    return "Hello " + string
+import torch
+class MiniBatchKmeans:
+    def __init__(self, n_clusters,batch_size,n_iter):
+        self.n_clusters = n_clusters
+        self.batch_size = batch_size
+        self.n_iter = n_iter
+    def forward(self, input):
+        samples, features = input.shape
+        # initialize centroid for each cluster
+        n_centroids = input[torch.randint(0,samples,(self.n_clusters,))] 
+        # v represent the number of data point for each cluster
+        v = torch.zeros(self.n_clusters)
+        for i in range(self.n_iter):
+            # randomly select batch_size indices for creating the current batch
+            batch = torch.randint(0, samples, (self.batch_size,))
+            input_batch = input[batch]
+            # compute distances from batch samples to centroids
+            d = torch.cdist(input_batch, n_centroids)
+            # find the nearest centroid for each sample in the batch
+            near_centroid = torch.argmin(d,dim=1)
+            # update the centroids based on samples
+            for id, x in enumerate(input_batch):    
+                c = near_centroid[id]
+                v[c] +=1
+                lr = 1/v[c]
+                n_centroids[c] = (1-lr)*n_centroids[c]+lr*x
+        return n_centroids
--- practical/DeepClustering/tests/test_deep_clustering_dummy.py
+++ practical/DeepClustering/tests/test_deep_clustering_dummy.py
@@ -1,5 +1,22 @@
-from practical.DeepClustering import hello_world
+from practical.DeepClustering import MiniBatchKmeans
+import torch
 
+# generate the input, 50 data poing with 10 features for each data point
+input  = torch.randn(50,10)
+n_clusters = 5
+batch_size = 10
+n_iter = 10
+model = MiniBatchKmeans(n_clusters,batch_size,n_iter)
+centers = model.forward(input)
+print(centers)
 
-def test_hello_world_clustering():
-    assert hello_world("Deep Clustering") == "Hello Deep Clustering"
+# output :tensor([[ 0.0802, -0.3703,  0.3922, -1.2998,  0.5223, -0.4484,  0.0399,  0.1811,
+#          -0.4285,  0.4424],
+#         [-0.8336,  1.0182,  0.4681,  0.1337,  0.0667,  0.3909, -0.1763,  0.2117,
+#          -1.7768, -0.6515],
+#         [ 0.1637,  0.0533,  0.1354, -0.2415,  0.9188, -0.0394,  1.0039, -0.8679,
+#           1.4603,  0.2197],
+#         [-0.0606,  0.2250, -0.2642,  1.0825,  0.7565, -1.5832, -1.6241, -1.0770,
+#          -0.3406, -1.6672],
+#         [ 0.4951, -0.3880,  0.2946,  1.2581,  0.1129, -0.0109, -0.5472,  0.1261,
+#           0.0413, -0.1444]])
