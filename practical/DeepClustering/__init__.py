7efdcca42cd1eb3e1f7750724ae1be3853a38170
Collin Leiber <33159895+collinleiber@users.noreply.github.com>
2024-04-29 14:46:59 +0200

    Update __init__.py

--- practical/DeepClustering/__init__.py
+++ practical/DeepClustering/__init__.py
@@ -1,5 +1,5 @@
 from practical.DeepClustering.deep_clustering_dummy import hello_world
-from practical.DeepClustering.deep_clustering_dummy import minibatch_k_means
+from practical.DeepClustering.deep_clustering_dummy import minibatch_k_means_khang_van
 
 
-__all__ = ["hello_world", "minibatch_k_means"]
+__all__ = ["hello_world", "minibatch_k_means_khang_van"]
