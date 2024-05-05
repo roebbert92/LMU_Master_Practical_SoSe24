d932de6b50248302277c64bf6401b25b886b26a9
handler-bird <powerk11@hotmail.de>
2024-05-05 15:49:19 +0200

    refactor the method into a class

--- practical/DeepClustering/deep_clustering_dummy.py
+++ practical/DeepClustering/deep_clustering_dummy.py
@@ -5,19 +5,59 @@ def hello_world(string: str) -> str:
     return "Hello " + string
 
 
-def minibatch_k_means_khang_van(k: int, size: int, iterations: int, X):
-    v = torch.zeros(k)
-    centroids = X[torch.randint(0, len(X), (k,))]
+class MiniBatchKMeans:
+    def __init__(self, k: int, batch_size: int, iterations: int):
+        if not isinstance(k, int):
+            raise TypeError("k should be of type integer")
+        if not isinstance(batch_size, int):
+            raise TypeError("batch_size should be of type integer")
+        if not isinstance(iterations, int):
+            raise TypeError("iterations should be of type integer")
+        if k < 1:
+            raise ValueError("number of clusters must be bigger than 0")
+        if batch_size < 1:
+            raise ValueError("batch size must be bigger than 0")
+        if iterations < 1:
+            raise ValueError("iteration must be bigger than 0")
 
-    for iteration in range(iterations):
-        batch_index = torch.randint(0, len(X), (size,))
-        batch = X[batch_index]
+        self.k = k
+        self.batch_size = batch_size
+        self.iterations = iterations
+
+    def distance(self, batch: torch.Tensor, centroids: torch.Tensor) -> torch.Tensor:
+        return torch.argmin(torch.cdist(batch, centroids), dim=1)
+
+    def fit(self, x: torch.Tensor) -> torch.Tensor:
+        center_counts = torch.zeros(self.k)
+        centroids = x[torch.randint(0, len(x), (self.k,))]
+
+        for _ in range(self.iterations):
+            batch_index = torch.randint(0, len(x), (self.batch_size,))
+            batch = x[batch_index]
+            distance = self.distance(batch, centroids)
+
+            for index, point in enumerate(batch):
+                center = distance[index]
+                center_counts[center] += 1
+                learning_rate = 1 / center_counts[center]
+                centroids[center] = (1 - learning_rate) * centroids[center] + learning_rate * point
+
+        return centroids
+
+
+def minibatch_k_means_khang_van(k: int, size: int, iterations: int, x: torch.Tensor) -> torch.Tensor:
+    center_counts = torch.zeros(k)
+    centroids = x[torch.randint(0, len(x), (k,))]
+
+    for _ in range(iterations):
+        batch_index = torch.randint(0, len(x), (size,))
+        batch = x[batch_index]
         distance = torch.argmin(torch.cdist(batch, centroids), dim=1)
 
         for index, point in enumerate(batch):
             center = distance[index]
-            v[center] += 1
-            learning_rate = 1 / v[center]
+            center_counts[center] += 1
+            learning_rate = 1 / center_counts[center]
             centroids[center] = (1 - learning_rate) * centroids[center] + learning_rate * point
 
     return centroids
