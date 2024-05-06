d3711931cda4a80c8bab800168a175807c074b74
Collin Leiber <33159895+collinleiber@users.noreply.github.com>
2024-05-06 14:30:53 +0200

    Update test_minibatch_kmeans.py

--- practical/DeepClustering/robin_loebbert/tests/test_minibatch_kmeans.py
+++ practical/DeepClustering/robin_loebbert/tests/test_minibatch_kmeans.py
@@ -1,6 +1,6 @@
 from practical.DeepClustering.robin_loebbert import MiniBatchKMeans
 from sklearn.datasets import load_wine
-from clustpy.metrics.clustering_metrics import unsupervised_clustering_accuracy
+from sklearn.metrics import adjusted_rand_score
 from sklearn.cluster import MiniBatchKMeans as skminibatch
 import time
 
@@ -10,21 +10,21 @@ def test_minibatch_kmeans():
     start_time = time.perf_counter()
     kmeans = MiniBatchKMeans(10, 1024, 20, device="cuda")
     pred_labels = kmeans.fit(data).predict(data)
-    print("my train acc: ", unsupervised_clustering_accuracy(labels, pred_labels))
+    print("my train acc: ", adjusted_rand_score(labels, pred_labels))
     print("duration: ", time.perf_counter() - start_time)
     start_time = time.perf_counter()
     sk_kmeans = skminibatch(
         10, init="random", batch_size=1024, max_iter=20, random_state=42
     )
     pred_labels = sk_kmeans.fit_predict(data)
-    print("sklearn train_acc: ", unsupervised_clustering_accuracy(labels, pred_labels))
+    print("sklearn train_acc: ", adjusted_rand_score(labels, pred_labels))
     print("duration: ", time.perf_counter() - start_time)
     test, test_labels = load_fmnist("test")
     print(
         "my test acc: ",
-        unsupervised_clustering_accuracy(test_labels, kmeans.predict(test)),
+        adjusted_rand_score(test_labels, kmeans.predict(test)),
     )
     print(
         "sklearn test acc: ",
-        unsupervised_clustering_accuracy(test_labels, sk_kmeans.predict(test)),
+        adjusted_rand_score(test_labels, sk_kmeans.predict(test)),
     )
